43897757c2aebab2f31fe1209769163c39fd6a59
ndd <nlpdumdum@gmail.com>
2022-12-25 13:43:56 +0800

    Reorganise torch imports

--- models/softmax_gate_trainer_wrapper.py
+++ models/softmax_gate_trainer_wrapper.py
@@ -2,8 +2,6 @@ from dataclasses import dataclass
 from typing import Optional, Tuple
 
 import torch
-import torch.nn as nn
-from torch.nn import CrossEntropyLoss
 from transformers import PreTrainedModel, PretrainedConfig
 from transformers.utils import ModelOutput
 
@@ -54,17 +52,17 @@ class SoftmaxGateTrainingPreTrainedModel(PreTrainedModel):
 
     def _init_weights(self, module):
         """Initialize the weights"""
-        if isinstance(module, nn.Linear):
+        if isinstance(module, torch.nn.Linear):
             # Slightly different from the TF version which uses truncated_normal for initialization
             # cf https://github.com/pytorch/pytorch/pull/5617
             module.weight.data.normal_(mean=0.0, std=self.config.initializer_range)
             if module.bias is not None:
                 module.bias.data.zero_()
-        elif isinstance(module, nn.Embedding):
+        elif isinstance(module, torch.nn.Embedding):
             module.weight.data.normal_(mean=0.0, std=self.config.initializer_range)
             if module.padding_idx is not None:
                 module.weight.data[module.padding_idx].zero_()
-        elif isinstance(module, nn.LayerNorm):
+        elif isinstance(module, torch.nn.LayerNorm):
             module.bias.data.zero_()
             module.weight.data.fill_(1.0)
 
@@ -115,7 +113,7 @@ class SoftmaxGateTraining(SoftmaxGateTrainingPreTrainedModel):
         loss = None
 
         if labels is not None:
-            loss_fct = CrossEntropyLoss()
+            loss_fct = torch.nn.CrossEntropyLoss()
             loss = loss_fct(logits.view(-1, self.num_labels), labels.view(-1))
 
         # if not return_dict:
